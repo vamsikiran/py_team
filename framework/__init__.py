e51eca7b3cdfb97603107cffae2ff3773fd6fb83
Ratanraj Ravuri <ratanraj.r@gmail.com>
2014-08-29 04:32:36 +0530

    Basic Framework in progress

--- framework/__init__.py
+++ framework/__init__.py
@@ -3,8 +3,9 @@ The Framework module
 """
 from django.http.response import HttpResponseNotFound, HttpResponseBadRequest,\
     Http404
+from weber.settings import weber_db
 
-class WeberView:
+class WeberView(object):
     def __init__(self, request):
         self.request=request
 
@@ -22,3 +23,23 @@ class WeberView:
             return cls(request).get(*args, **kwargs)
         else:
             return HttpResponseBadRequest("Invalid Request")
+
+
+class Model(object):
+    collection_name = None
+    def __init__(self, *args, **kwargs):
+        pass
+    def save(self):
+        pass
+    def encode(self):
+        pass
+    def decode(self):
+        pass
+
+    @classmethod
+    def getByFieldValue(cls, field, value):
+        attrs = weber_db.find_document(cls.collection_name, field, value)
+        if attrs:
+            return cls(attrs)
+        else:
+            None
--- weber/settings.py
+++ weber/settings.py
@@ -10,6 +10,7 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+from framework.weberdb import WeberDB
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 
@@ -51,6 +52,9 @@ MIDDLEWARE_CLASSES = (
 ROOT_URLCONF = 'weber.urls'
 
 WSGI_APPLICATION = 'weber.wsgi.application'
+
+
+weber_db = WeberDB()
 
 
 # Database
