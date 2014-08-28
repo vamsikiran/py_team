ada03eb28ff37118be25c7df40cf7d41610f31d6
Ratanraj Ravuri <ratanraj.r@gmail.com>
2014-08-29 03:19:30 +0530

    Framework

---
+++ framework/__init__.py
@@ -0,0 +1,24 @@
+"""
+The Framework module
+"""
+from django.http.response import HttpResponseNotFound, HttpResponseBadRequest,\
+    Http404
+
+class WeberView:
+    def __init__(self, request):
+        self.request=request
+
+    def get(self, *args, **kwargs):
+        raise Http404
+
+    def post(self, *args, **kwargs):
+        return Http404
+
+    @classmethod
+    def as_view(cls, request, *args, **kwargs):
+        if request.method=='GET':
+            return cls(request).get(*args, **kwargs)
+        elif request.method=='POST':
+            return cls(request).get(*args, **kwargs)
+        else:
+            return HttpResponseBadRequest("Invalid Request")
