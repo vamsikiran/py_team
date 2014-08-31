f82aec7e36a6c0691cc7f0c84c2a77f4a632ff60
Ratanraj Ravuri <ratanraj.r@gmail.com>
2014-08-31 23:44:00 +0530

    domainmodel auto attrs

--- domainmodel/__init__.py
+++ domainmodel/__init__.py
@@ -3,7 +3,8 @@ from weber.settings import weber_db
 class Model(object):
     collection_name = None
     def __init__(self, *args, **kwargs):
-        pass
+        self.attrs = kwargs.get('attrs',{})
+        
     def save(self):
         pass
     def encode(self):
@@ -11,11 +12,17 @@ class Model(object):
     def decode(self):
         pass
 
+    def __getattr__(self,attr_name):
+        try:
+            return self.attrs['object'][attr_name]
+        except:
+            raise AttributeError
+
     @classmethod
     def getByFieldValue(cls, field, value):
         attrs = weber_db.find_document(cls.collection_name, field, value)
         if attrs:
-            return cls(attrs)
+            return cls(attrs=attrs)
         else:
             None
 
