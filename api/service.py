3bf815e828c7ea2e5a9661c5988921994755125e
Ratanraj Ravuri <ratanraj.r@gmail.com>
2014-08-31 23:43:19 +0530

    api/service

--- api/service.py
+++ api/service.py
@@ -1,9 +1,10 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, StreamingHttpResponse
 from django.conf.urls import patterns, include, url
 from framework import WeberView
 import json
 from django.http.response import Http404
 from weber.settings import weber_db
+import time
 
 
 class Service(WeberView):
@@ -23,6 +24,14 @@ class Service(WeberView):
             raise Http404('Command Not Found')
         return HttpResponse(responseText)
 
+class Test(WeberView):
+    def get(self, *args, **kwargs):
+        def test(n):
+            for i in range(int(n)):
+                yield(str(i))
+        return StreamingHttpResponse(test(kwargs.get('n',10)))
+
 urlpatterns = patterns('',
-url(r'^service/?(?P<command>[A-Za-z0-9]*)/$',Service.as_view,name='service'),
+url(r'^service/?(?P<command>[A-Za-z]*)$',Service.as_view,name='service'),
+url(r'^service/test/?(?P<n>[0-9]*)$',Test.as_view, name='test'),
 )
